e5a0b8a4af0825564df13ed021dd25bbea14e704
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-06-02 12:25:36 +0545

    combining core data and swiftUI

--- project 11/ContentView.swift
+++ project 11/ContentView.swift
@@ -6,15 +6,33 @@
 //
 
 import SwiftUI
-import CoreData
-
-
-
 
 struct ContentView: View {
-
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    
     var body: some View {
-     SizeClasses()
+        VStack {
+            List {
+                ForEach(students, id: \.id){ student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+            
+            Button("Add") {
+                let firstNames = [ "Ginny", "Anish", "Shyam" , "Reeta"]
+                let lastnames = ["Huds", "Anstan", "Shakesphere", "Swift"]
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastnames.randomElement()
+                
+                
+//                creating object for an Student object from core data
+                let student = Student(context: self.moc)
+                student.id  = UUID()
+                student.name = "\(chosenFirstName)! \(chosenLastName)!"
+                try? self.moc.save()
+            }
+        }
     }
 }
 
